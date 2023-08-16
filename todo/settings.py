24ba37466b023c311c96c889f51df833a49145e4
Nika <nurilaassyk1994@gmail.com>
2023-08-16 20:05:57 +0600

    Создала приложение для todo

--- todo/settings.py
+++ todo/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "todo",
+        "USER": "postgres",
+        "PASSWORD": "170303",
+        "HOST": "localhost",
+        "PORT": ""
     }
 }
 

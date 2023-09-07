6704f25c84667d358a20f6ca3687e33206ecae10
Нурила <113448331+nurilaassyk@users.noreply.github.com>
2023-09-07 13:26:14 +0600

    Revert "Создала инструкции для запуска проекта локально на docker"

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.10.7
-
-ENV PYTHONUNBUFFERED=1
-WORKDIR /code
-COPY requirements.txt .
-RUN pip install --no-cache -r requirements.txt
-COPY . .
-EXPOSE 8000
-
--- todo/settings.py
+++ todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,8 +83,8 @@ DATABASES = {
         "NAME": "todo",
         "USER": "postgres",
         "PASSWORD": "170303",
-        "HOST": os.environ.get('DB_HOST', 'db_host'),
-        "PORT": 5432
+        "HOST": "localhost",
+        "PORT": ""
     }
 }
 
@@ -120,13 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-
-STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_URL = '/media/'
-LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'login'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
